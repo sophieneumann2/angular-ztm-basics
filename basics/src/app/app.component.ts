8dceeae9c16618185a87c2db67153c7f71177a8a
Sophie Neumann <sn@evergreen.de>
2022-11-20 19:54:52 +0100

    basic property binding

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'basics';
+  name = 'Sophie';
+  imgUrl =
+    'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0';
+
+  getName() {
+    return this.name;
+  }
 }
